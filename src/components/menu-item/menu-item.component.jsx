3dc32d452fc3afe10feced1b40655593e6858b70
Alexej Holad <alexej.holad@gmail.com>
2021-12-25 12:36:15 +0100

    update our menu-items to navigate with useNavigate() Hook

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { useNavigate } from "react-router";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  let navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`${linkUrl}`);
+  };
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={handleClick}>
       <div
-        className='background-image' style={{
+        className="background-image"
+        style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
